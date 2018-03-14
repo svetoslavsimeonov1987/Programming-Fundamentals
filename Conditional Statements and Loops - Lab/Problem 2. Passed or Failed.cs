bda96cb7bccc01e9c00ee77118a24c9d0c04aba5
Svetoslav Simeonov <svetoslavsimeonov@abv.bg>
2018-03-14 13:58:31 +0200

    aded

---
+++ Conditional Statements and Loops - Lab/Problem 2. Passed or Failed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HomeworkConditionalStatements
+{
+
+    class PassedFailed
+    {
+        static void Main()
+        {
+            var input = double.Parse(Console.ReadLine());
+
+            if (input >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
