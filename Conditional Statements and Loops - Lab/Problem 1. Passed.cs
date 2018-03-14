46803cca19726aebd55f0370a65c9f7f1891b405
Svetoslav Simeonov <svetoslavsimeonov@abv.bg>
2018-03-14 14:01:11 +0200

    passed

---
+++ Conditional Statements and Loops - Lab/Problem 1. Passed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeworkConditionalStatements
+{
+
+    class Passed
+    {
+        static void Main()
+        {
+            var input = double.Parse(Console.ReadLine());
+
+            if (input >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
