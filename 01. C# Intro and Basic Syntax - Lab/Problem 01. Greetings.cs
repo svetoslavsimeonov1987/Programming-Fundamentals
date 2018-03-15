866c902a3f5de729cc285133c1738dc6c39e44d6
Svetoslav Simeonov <svetoslav.r.simeonov@gmail.com>
2018-03-15 20:07:30 +0200

    Problem 01. Greetings.cs added

---
+++ 01. C# Intro and Basic Syntax - Lab/Problem 01. Greetings.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace Homework
+{
+    class Greetings
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
